e83a7bbe433ec4cc4498747d028aa33ac3270e4c
HyperPickle <rishiaussie@gmail.com>
2026-07-15 23:25:50 +1000

    Initial Commit

---
+++ Sipped/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Sipped
+//
+//  Created by Rishi on 15/7/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
